87dbddac3ad3a9282e7632637452b98d1c3e6cb5
Enes Uraz <axeprime91@gmail.com>
2023-04-26 21:45:42 +0300

    craete router functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import Categories from "./components/categories/Categories";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 const App = () => {
-  const categories = [
-    {
-      id: 2,
-      title: "Women",
-      image:
-        "https://images.unsplash.com/photo-1532453288672-3a27e9be9efd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    },
-    {
-      id: 1,
-      title: "Men",
-      image:
-        "https://images.unsplash.com/photo-1513373319109-eb154073eb0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fG1lbiUyMGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    },
-    {
-      id: 3,
-      title: "Kids",
-      image:
-        "https://images.unsplash.com/photo-1556905055-8f358a7a47b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGtpZHMlMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    },
-  ];
-
-  return <Categories categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
